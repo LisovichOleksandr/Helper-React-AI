626c03f1d539036011495d16886b59c23461c155
alexander lisovich <lisovyhs@gmail.com>
2026-01-16 12:10:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [activeTab, setActiveTab] = useState('image-generator');
+
+  const handleTabChange = (tab) => {
+    alert(tab);
+    setActiveTab(tab);  
+  
+  }
+    return (
+    <div className='App'>
+      <button onClick={() =>handleTabChange('image-generator')}>Image Generator</button>
+      <button onClick={() => handleTabChange('chat')}>Chat</button>
+      <button onClick={() => handleTabChange('recipe-generator')}>Recipe Generator</button>
+    </div>
+  )
+}
+
+export default App
